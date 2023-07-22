980c4414b252bde7b76cad295a78c545d7a56301
LiChunMing <simonli123hk@gmail.com>
2023-07-22 20:31:45 +0800

    just a commit

--- vscodeextensioninstall.ps1
+++ vscodeextensioninstall.ps1
@@ -1,57 +1,34 @@
-# Error log
-$errorlog = "c:\temp\choco-install-errors.txt"
-
-# Check for admin rights
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-    Write-Error "Please run as administrator"
-    Exit
+#check is running as admin
+$admin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+if ($admin -eq $false) {
+    Write-Host "Please run as administrator"
+    exit
 }
 
-# Install Chocolatey if needed
-If (!(Get-Command choco -ErrorAction SilentlyContinue)) {
-    Write-Host "Installing Chocolatey..."
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+#check if chocolatey is installed
+if (Get-Command choco -ErrorAction SilentlyContinue) {
+    Write-Host "Chocolatey is installed"
+} else {
+    Write-Host "Chocolatey is not installed"
+    Write-Host "Installing Chocolatey"
+    #insatll chocolatey
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
-# Load software list 
-$software = Get-Content "chocoinstall.txt"
 
-# Install software
+# Read software list from file
+$software = Get-Content chocoinstall.txt
+
+# Install each one
 foreach ($app in $software) {
-
-    Write-Host "Installing $app"
-    
-    # Check if already installed
-    $installed = choco list -lo $app
-    if ($installed) {
-        Write-Host "$app is already installed. Skipping."
-        Continue
-    }
-
-    # Install app
-    try {
-        choco install $app -y
-    }
-    catch {
-        Write-Error "Error installing $app. $($_.Exception.Message)" | Out-File $errorlog -Append
-    }
-
+    choco install $app -y
 }
 
-# Install VS Code extensions
-$extensions = Get-Content "vscode_extensions.txt"
-
-foreach ($ext in $extensions) {
-    Write-Host "Installing $ext VS Code extension"
-    try {
-        code --install-extension $ext
+#install vscode extensions
+if (Get-Command code -ErrorAction SilentlyContinue) {
+    $vscodeextensions = Get-Content vscodeextensions.txt
+    foreach ($extension in $vscodeextensions) {
+        code --install-extension $extension
     }
-    catch {
-        Write-Error "Error installing $ext. $($_.Exception.Message)" | Out-File $errorlog -Append
-    }
-
 }
 
-Write-Host "Done!"
